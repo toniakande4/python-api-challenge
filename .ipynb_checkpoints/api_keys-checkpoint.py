415e9a3e07cbb0e4b6cc454f025244ec054583d1
Toni <toniakande4@gmail.com>
2024-11-20 03:41:28 -0600

    commit this

---
+++ .ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "635f80956f1746f3df720557e2453e08"
+
+# Geoapify API Key
+geoapify_key = "b5b51b62dd9988cafa8029fda63d3277"
--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "179cfa528931a16b808c9b9c8a6e71dc"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "efad68798e798d0a04f0bcd584169a3b"
